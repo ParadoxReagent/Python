daac2ab0ae96518df311b2d79e57e55d37eba2ab
ParadoxReagent <73672349+ParadoxReagent@users.noreply.github.com>
2021-09-16 11:47:30 -0400

    Update SSHBruteForce.py

--- SSHBruteForce.py
+++ SSHBruteForce.py
@@ -1,8 +1,10 @@
-from pwn impport *
+#!/usr/bin/env python
+
+from pwn import *
 import paramiko
 
-host = "127.0.0.1"
-username = "notroot"
+host = input("Please type in your targets IP address: ")	#user enters in host SSH IP
+username = input("Please type in your targets username: ")	#user enters in username they would like to use for brute force
 attempts = 0
 
 with open("ssh-common-passwords.txt", "r") as password_list:
