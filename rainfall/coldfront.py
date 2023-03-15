d9bfc7dfa23c791d114fe2d6b5cd3dd0ac3d6b6c
ParadoxReagent <73672349+ParadoxReagent@users.noreply.github.com>
2023-03-15 10:22:20 -0400

    Create coldfront.py

---
+++ rainfall/coldfront.py
@@ -0,0 +1,16 @@
+import os
+
+# Define a list of file extensions to delete
+extensions = ['.VHD', '.bac', '.bak', '.wbcat', '.bkf', '.set', '.win', '.dsk']
+
+# Define a list of filename patterns to delete
+patterns = ['Backup*.*', 'backup*.*']
+
+# Define the directory to delete files from
+directory = r'C:\path\to\directory'
+
+# Recursively delete all files matching the specified extensions and patterns
+for root, dirs, files in os.walk(directory):
+    for file in files:
+        if any(file.endswith(ext) for ext in extensions) or any(pattern in file for pattern in patterns):
+            os.remove(os.path.join(root, file))
