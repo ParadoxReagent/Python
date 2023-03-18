4b46b48336d782356ea2ae92df293d7c7442228c
ParadoxReagent <73672349+ParadoxReagent@users.noreply.github.com>
2023-03-17 23:41:20 -0400

    added more checks

--- rainfall/weathervane.py
+++ rainfall/weathervane.py
@@ -1,6 +1,8 @@
 import platform
 import psutil
 import subprocess
+import sys
+import ctypes
 
 
 def check_dmi_info(keywords):
@@ -32,7 +34,8 @@ def check_windows_vm():
             wmi_obj = wmi.WMI()
             for device_keyword in device_keywords:
                 for device in wmi_obj.query("SELECT * FROM Win32_PnPEntity WHERE Name LIKE '%" + device_keyword + "%'"):
-                    return True
+                    if device:
+                        return True
         except ImportError:
             pass
         return False
@@ -42,9 +45,9 @@ def check_windows_vm():
             import wmi
             wmi_obj = wmi.WMI()
             for service_keyword in service_keywords:
-                for service in wmi_obj.query(
-                        "SELECT * FROM Win32_Service WHERE DisplayName LIKE '%" + service_keyword + "%'"):
-                    return True
+                for service in wmi_obj.query("SELECT * FROM Win32_Service WHERE DisplayName LIKE '%" + service_keyword + "%'"):
+                    if service:
+                        return True
         except ImportError:
             pass
         return False
@@ -59,7 +62,8 @@ def check_windows_vm():
         import wmi
         wmi_obj = wmi.WMI()
         for disk_drive in wmi_obj.query("SELECT * FROM Win32_DiskDrive WHERE Model LIKE 'Msft Virtual Disk%'"):
-            return True
+            if disk_drive:
+                return True
     except ImportError:
         pass
 
@@ -130,6 +134,112 @@ def is_virtual_machine():
         return check_macos_vm()
 
     return False
+
+
+############# Check for sandbox ################
+# if platform.system() == 'Windows':
+#    import winreg
+#
+#
+#    def check_sandbox_artifacts():
+#        artifacts = [
+#            (winreg.HKEY_LOCAL_MACHINE, r'SOFTWARE\Oracle\VirtualBox Guest Additions'),
+#            (winreg.HKEY_LOCAL_MACHINE, r'SYSTEM\CurrentControlSet\Services\VBoxGuest'),
+#            (winreg.HKEY_LOCAL_MACHINE, r'SYSTEM\CurrentControlSet\Services\VBoxMouse'),
+#            (winreg.HKEY_LOCAL_MACHINE, r'SYSTEM\CurrentControlSet\Services\VBoxService'),
+#            (winreg.HKEY_LOCAL_MACHINE, r'SYSTEM\CurrentControlSet\Services\VBoxSF'),
+#            (winreg.HKEY_LOCAL_MACHINE, r'SYSTEM\CurrentControlSet\Services\VBoxVideo'),
+#            (winreg.HKEY_LOCAL_MACHINE, r'SYSTEM\CurrentControlSet\Services\vmicheartbeat'),
+#            (winreg.HKEY_LOCAL_MACHINE, r'SYSTEM\CurrentControlSet\Services\vmicvss'),
+#            (winreg.HKEY_LOCAL_MACHINE, r'SYSTEM\CurrentControlSet\Services\vmicshutdown'),
+#            (winreg.HKEY_LOCAL_MACHINE, r'SYSTEM\CurrentControlSet\Services\vmicexchange'),
+#            # ... (add more sandbox-specific registry keys)
+#        ]
+#
+#        for root, key_path in artifacts:
+#            try:
+#                with winreg.OpenKey(root, key_path):
+#                    return True
+#            except FileNotFoundError:
+#                pass
+#
+#        return False
+#
+#    if check_sandbox_artifacts():
+#        print("Sandbox artifact detected. Exiting.")
+#        sys.exit(1)  # Exit the script with an error code
+
+
+def check_low_system_entropy():
+    import os
+
+    entropy = os.urandom(64)
+    unique_byte_count = len(set(entropy))
+
+    return unique_byte_count < 40
+
+
+if check_low_system_entropy():
+    print("Low system entropy detected. Exiting.")
+    sys.exit(1)  # Exit the script with an error code
+
+
+def check_analysis_tools():
+    analysis_tools = [
+        'tcpdump', 'Wireshark', 'fiddler', 'burpsuite', 'charles',
+        'idaq.exe', 'idaq64.exe', 'ollydbg', 'windbg', 'gdb',
+        'x64dbg', 'x32dbg', 'x64bdg', 'x32bdg', 'ImmunityDebugger',
+        'radare2', 'strace', 'ltrace', 'dnSpy', 'de4dot', 'ghidra'
+    ]
+
+    for process in psutil.process_iter(['name']):
+        try:
+            process_name = process.info['name']
+            if process_name in analysis_tools:
+                return True
+        except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
+            pass
+
+    return False
+
+
+if check_analysis_tools():
+    print("Analysis tool detected. Exiting.")
+    sys.exit(1)  # Exit the script with an error code
+
+
+def is_debugger_attached_windows():
+    kernel32 = ctypes.windll.kernel32
+    return kernel32.IsDebuggerPresent() != 0
+
+
+def is_debugger_attached_linux():
+    try:
+        with open('/proc/self/status', 'r') as status_file:
+            for line in status_file:
+                if line.startswith('TracerPid:'):
+                    tracer_pid = int(line.split(':', 1)[1].strip())
+                    return tracer_pid != 0
+    except FileNotFoundError:
+        pass
+    return False
+
+
+def is_debugger_attached():
+    os_name = platform.system()
+
+    if os_name == 'Windows':
+        return is_debugger_attached_windows()
+    elif os_name == 'Linux':
+        return is_debugger_attached_linux()
+    else:
+        # We are not checking for debuggers on other platforms
+        return False
+
+
+if is_debugger_attached():
+    print("Debugger detected. Exiting.")
+    sys.exit(1)  # Exit the script with an error code
 
 
 if is_virtual_machine():
